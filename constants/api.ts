ad00223d27a407ff169501f3cdc88f675aae32b1
Achraf Saidi <achraf-saidi@outlook.com>
2026-04-29 22:12:59 +0100

    `Updated app.json and removed unused images`

--- constants/api.ts
+++ constants/api.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { Platform } from "react-native";
 
-const LOCAL_API_URL = Platform.select({
-  android: "http://192.168.80.1:3000/api/v1",
-  ios: "http://192.168.80.1:3000/api/v1",
-  default: "http://localhost:3000/api/v1",
-});
-
+// const LOCAL_API_URL = Platform.select({
+//   android: "http://192.168.80.1:3000/api/v1",
+//   ios: "http://192.168.80.1:3000/api/v1",
+//   default: "http://localhost:3000/api/v1",
+// });
+const LOCAL_API_URL = "https://ecommerce-app-backend-server.vercel.app/api/v1";
 console.log("API Base URL:", LOCAL_API_URL);
 
 const api = axios.create({
